727c5cb2464947c9a5f4da0afdde910eefce6b82
Jack Warner <jack.warner@corporateaccess.net>
2015-11-09 10:53:48 -0500

    Reflect version on admin menu

--- application/views/admin/super/adminmenu.php
+++ application/views/admin/super/adminmenu.php
@@ -11,7 +11,7 @@
                 </strong>
                 <?php } ?>
         </div>
-        <div class='menubar-title-right'><a href='<?php echo "http://www.watsonresearchwiki.org/change-history";?>' target="_blank"><?php echo "Watson (Version RC18)";?></a></div>
+        <div class='menubar-title-right'><a href='<?php echo "http://www.watsonresearchwiki.org/change-history";?>' target="_blank"><?php echo "Watson (Version RC19)";?></a></div>
     </div>
     <div class='menubar-main'>
         <div class='menubar-left'>
