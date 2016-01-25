9c70d0a527629258af21d3ee640412a6998fe742
Jack Warner <jack.warner@corporateaccess.net>
2016-01-25 13:03:06 -0500

    New build and RC

--- application/config/version.php
+++ application/config/version.php
@@ -14,7 +14,7 @@
 
 $config['versionnumber'] = "2.06+";
 $config['dbversionnumber'] = 184;
-$config['buildnumber'] = '160121';
+$config['buildnumber'] = '160123';
 $config['updatable'] = true;
 
 return $config;
